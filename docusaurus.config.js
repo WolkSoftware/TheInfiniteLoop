c5c1f83d44b1b4216d6ab77adbc94952afa4ded2
Remo Jansen <remojansen@REMOS-MBP.station>
2023-02-23 10:47:35 +0000

    WIP analytics + domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,12 +38,15 @@ const config = {
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        gtag: {
+          trackingID: "G-D1C8FD9HV0",
+          anonymizeIP: true,
+        },
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/WolkSoftware/TheInfiniteLoop/tree/main/",
           path: "guide",
           routeBasePath: "guide",
         },
@@ -51,8 +54,9 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/WolkSoftware/TheInfiniteLoop/tree/main/",
+          path: "blog",
+          routeBasePath: "blog",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
